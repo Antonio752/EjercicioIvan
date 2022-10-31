e0eb4556c5ccc9756e882b1de5ac035749534d0c
Antonio752 <antonioblancoteixeira1@gmail.com>
2022-10-31 19:44:31 +0100

    Ejercicio de Ivan

---
+++ src/ejercicio1/Presa.java
@@ -0,0 +1,11 @@
+package ejercicio1;
+
+public abstract class Presa {
+    
+    public String alimentar(String alimento){
+        
+        String comida = alimento;
+        return "Me alimento de " + comida;       
+    }
+    
+}
